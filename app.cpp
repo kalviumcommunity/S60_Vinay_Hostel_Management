2f116cdd912cfeb879632c9f2ac0d6ccff38baec
Vinnu Gollakoti <vinnugollakoti@Vinnus-MacBook-Pro.local>
2024-08-30 08:29:58 +0530

    added static member function

--- app.cpp
+++ app.cpp
@@ -20,6 +20,9 @@ public:
     Hostels(string name, int rooms, string type, bool bookedStatus)
         : hostelName(name), noOfRooms(rooms), bedType(type), isBooked(bookedStatus) {
         totalHostels++;
+        if (isBooked) {
+            totalBookedRooms++;
+        }
     }
 
     ~Hostels() {
@@ -81,35 +84,36 @@ private:
 
 int main() {
     const int numHostels = 3;
-        Hostels* hostels[numHostels];
-        
-        hostels[0] = new Hostels("Mens hotel 1", 50, "Single", false);
-        hostels[1] = new Hostels("Mens hotel 2", 1, "Double", false);
-        hostels[2] = new Hostels("Ladies hotel 1", 20, "Double", true);
-        
-        for (int i = 0; i < numHostels; ++i) {
-            cout << "Displaying hostel capacities fully \n";
-            hostels[i]->displayHostel();
-        }
-        
-        for (int i = 0; i < numHostels; ++i) {
-            cout << "Attempting to book a room in " << (hostels[i]->isAvailable() ? "available hostel" : "unavailable hostel") << endl;
-            hostels[i]->bookRoom();
-        }
-        
-        for (int i = 0; i < numHostels; ++i) {
-            cout << "Displaying of rooms available in each hostels \n";
-            hostels[i]->displayHostel();
-        }
-
-        for (int i = 0; i < numHostels; ++i) {
-            cout << "Attempting to book a room in " << (hostels[i]->isAvailable() ? "available hostel" : "unavailable hostel") << endl;
-            hostels[i]->bookRoom();
-        }
-        Hostels::displayStats();
-        for (int i = 0; i < numHostels; ++i) {
-            delete hostels[i];
-        }
-
-        return 0;
+    Hostels* hostels[numHostels];
+    
+    hostels[0] = new Hostels("Mens hotel 1", 50, "Single", false);
+    hostels[1] = new Hostels("Mens hotel 2", 1, "Double", false);
+    hostels[2] = new Hostels("Ladies hotel 1", 20, "Double", true);
+    
+    for (int i = 0; i < numHostels; ++i) {
+        cout << "Displaying hostel capacities fully:\n";
+        hostels[i]->displayHostel();
     }
+    
+    for (int i = 0; i < numHostels; ++i) {
+        cout << "Attempting to book a room in " << (hostels[i]->isAvailable() ? "available hostel" : "unavailable hostel") << endl;
+        hostels[i]->bookRoom();
+    }
+    
+    for (int i = 0; i < numHostels; ++i) {
+        cout << "Displaying updated hostel details:\n";
+        hostels[i]->displayHostel();
+    }
+
+    for (int i = 0; i < numHostels; ++i) {
+        cout << "Attempting to book a room in " << (hostels[i]->isAvailable() ? "available hostel" : "unavailable hostel") << endl;
+        hostels[i]->bookRoom();
+    }
+    
+    Hostels::displayStats();
+
+    for (int i = 0; i < numHostels; ++i) {
+        delete hostels[i];
+    }
+    return 0;
+}
